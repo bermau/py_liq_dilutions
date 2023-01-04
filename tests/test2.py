5be66e4ade924e3ea18ddf41005f30254146e45c
Bert13 <bertrand.maubert@ch-lepuy.fr>
2023-01-04 09:28:51 +0100

    élimination de lignes inutiles

--- tests/test2.py
+++ tests/test2.py
@@ -1,5 +1,6 @@
 import unittest
-from  liquid_dilutions import *
+from liquid_dilutions import *
+
 
 class MyTestCase(unittest.TestCase):
 
@@ -14,21 +15,19 @@ class MyTestCase(unittest.TestCase):
 
     def test_04_preparer_dilution_de_ct_imposant_dilutions(self):
         print(f"\nSOLUTION DEPART {self.tube_ct_mere} \n")
-        fille0 = diluer(self.tube_ct_mere, 10, volume_final=300, tag='la_fille', comment= True)
-        res = preparer(220, 35, self.tube_ct_mere,  n_dil=3, lst_imposed_dil=[[100,200],[150,210]], comment=True  )
+        res = preparer(220, 35, self.tube_ct_mere, n_dil=3, lst_imposed_dil=[[100, 200], [150, 210]], comment=True)
         print()
         print(res)
 
     def test_06_preparer_dilution_de_ct(self):
         print(f"\nSOLUTION DEPART {self.tube_ct_mere} \n")
-        fille0 = diluer(self.tube_ct_mere, 10, volume_final=300, tag='la_fille', comment= True)
         res = preparer(200, 35, self.tube_ct_mere, comment=True)
         print()
         print(res)
 
     def test_08_dilution_ct_en_fille(self):
         print(self.tube_ct_mere)
-        fille0 = diluer(self.tube_ct_mere, 10, volume_final=300, tag='la_fille', comment= True)
+        fille0 = diluer(self.tube_ct_mere, 10, volume_final=300, tag='la_fille', comment=True)
         print(fille0)
 
     def test_10_dilution_ct_par_defaut(self):
@@ -45,6 +44,6 @@ class MyTestCase(unittest.TestCase):
     # self.assertAlmostEqual(T.height_for_volume(3000), 38.20, 2)
     # self.assertAlmostEqual(T.height_for_volume(500), 6.37, 2)
 
+
 if __name__ == '__main__':
     unittest.main()
-
