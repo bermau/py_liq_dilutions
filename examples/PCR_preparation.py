d3b5e51bcdbf75e49835e3f9e9ce676dad5c1b6c
Bert13 <bertrand.maubert@ch-lepuy.fr>
2023-01-07 09:46:39 +0100

    first commit : exemple d'utilisation

--- examples/PCR_preparation.py
+++ examples/PCR_preparation.py
@@ -1,10 +1,10 @@
 """Exemple d'utilisation """
 from liquid_dilutions import *
 
-mater = Liquid("VRS")
+mater = Liquid("COVID JAL/BEA")
 diluent = Liquid("UTM")
 
-tube_ct_mere = Aliquot(mater, volume=500, ct=20.5, unit_type='ct')
+tube_ct_mere = Aliquot(mater, volume=500, ct=18, unit_type='ct')
 
 # On a un CT à 20,5 et on veux l'amener vers 33
 
@@ -13,7 +13,8 @@ print(f"""\nSOLUTION DEPART :
   ** {tube_ct_mere} 
   ** Caractéristiques : {tube_ct_mere.describe()}
        """)
-dil_imposes = [[25, 200]]  # [[dilution, volume]  ...]
+
+dil_imposes = [[30, 200]]  # [[dilution, volume]  ...]
 
 # res = preparer(vf=3, ct_cc_cible=30, tube=tube_ct_mere, n_dil=3,
 #                lst_imposed_dil=dil_imposes, comment=True)
