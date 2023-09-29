966a0d0cbb60c3ecdeb4e075f93139a4dce5e5db
Bert13 <bertrand.maubert@ch-lepuy.fr>
2023-09-29 15:34:10 +0200

    Ok avant interface graphique

--- ipl_printer_label.py
+++ ipl_printer_label.py
@@ -1,26 +1,23 @@
-from local import my_printer
+from local import my_printer, printer_list
 import subprocess
 from datetime import datetime
 
-sNbreEtq = 1
+nb_labels = 3
 
-sNomPrenom = "TOTO Léon"
-sId1 = "25121245"
-sId2 = "98"
-sDatNaissSex = "31/12/1964 M"
+field_1 = "25121245"
+field_2= "98"
+field_3 = "TEST second"
+field_4 = "31/12/1964 M"
 sDatPrel = "15/02/2023"
 now = datetime.now().strftime("%Y/%m/%d %H:%M")
 
-# <STX>H03;o55,565;b1;f2;h01;w01;c34;d3," + [sIdIndex]+ ";<ETX>
-# /*<STX>H02;o180,565;b0;f2;h01;w01;c34;d3,{sId2};<ETX> */
-
-msg_mispl = f"""
+ipl_format_for_Epp_1_5_ml = f"""
 <STX><ESC>C<ETX><STX><ESC>P<ETX><STX>E5;F5;<ETX>
-<STX>H01;o315,565;b0;f2;h01;w01;c34;d3,{sId1};<ETX>
+<STX>H01;o315,565;b0;f2;h01;w01;c34;d3,{field_1};<ETX>
 
-<STX>H04;o315,520;b0;f2;h01;w01;c34;d3,{sNomPrenom};<ETX>
+<STX>H04;o315,520;b0;f2;h01;w01;c34;d3,{field_3};<ETX>
 
-<STX>H05;o315,455;b0;f2;h02;w01;c2;d3,{sDatNaissSex};<ETX>
+<STX>H05;o315,455;b0;f2;h02;w01;c2;d3,{field_4};<ETX>
 <STX>H06;o315,415;b0;f2;h01;w01;c30;d3,{now};<ETX>
 
 /* ligne */
@@ -31,14 +28,17 @@ msg_mispl = f"""
 <STX>H14;o315,300;b1;f2;h01;w01;c31;d3,BIOMOL;<ETX>
 
 /* Mini étiquette pour couvercle */
-<STX>H16;o315,100;b0;f2;h01;w01;c31;d3,{sId1};<ETX>
-<STX>H17;o315,65;b0;f2;h01;w01;c31;d3,{sNomPrenom};<ETX>
-<STX>R<ETX><STX><ESC>E5<CAN><ETX><STX><RS>{sNbreEtq}<ETB><ETX>
+<STX>H16;o315,100;b0;f2;h01;w01;c31;d3,{field_1};<ETX>
+<STX>H17;o315,65;b0;f2;h01;w01;c31;d3,{field_3};<ETX>
+<STX>R<ETX><STX><ESC>E5<CAN><ETX><STX><RS>{nb_labels}<ETB><ETX>
 """
 
-with open("etiq.txt", 'w') as f:
-    f.writelines(msg_mispl)
-# copy &1 my_printer
-# my_printer = "totu"
 
-script = subprocess.check_output(["copy", ".\etiq.txt", my_printer], shell=True)
+def print():
+    with open("etiq.txt", 'w') as f:
+        f.writelines(ipl_format_for_Epp_1_5_ml)
+
+    subprocess.check_output(["copy", ".\etiq.txt", my_printer], shell=True)
+
+if __name__ == '__main__':
+    print()
