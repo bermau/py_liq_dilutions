e0a626ee8eea78d821df9b9be8d91acf510824d2
Bert13 <bertrand.maubert@ch-lepuy.fr>
2023-12-15 18:05:26 +0100

    amélioration. Crée une image.

--- ipl_to_image.py
+++ ipl_to_image.py
@@ -1,10 +1,10 @@
 """Create an image from an IPL code"""
 
 
-field_1 = "231224"
-field_2 = "NOM Prénom"
-field_3 = "extraction"
-field_4 = "aliquot"
+field_1 = "25121245"
+field_2 = "98"
+field_3 = "TEST second"
+field_4 = "31/12/1964 M"
 now = "2023/23/15 10:23"
 nb_labels= 3
 
@@ -30,8 +30,7 @@ ipl_format_for_Epp_1_5_ml = f"""
 <STX>R<ETX><STX><ESC>E5<CAN><ETX><STX><RS>{nb_labels}<ETB><ETX>
 """
 
-
-
+import re
 from PIL import Image, ImageDraw, ImageFont
 
 def generate_image_from_ipl(ipl_code, image_path):
@@ -41,6 +40,12 @@ def generate_image_from_ipl(ipl_code, image_path):
 
     # Use a monospaced font for consistent spacing
     font = ImageFont.load_default()
+
+    # Remove comments delimited by /* */
+    ipl_code = re.sub(r'/\*.*?\*/', '', ipl_code, flags=re.DOTALL)
+
+    # Remove leading newlines from each command_type
+    ipl_code = ipl_code.replace('\n<', '<')
 
     # Split the IPL code by <ETX> to get individual commands
     commands = ipl_code.split("<ETX>")
@@ -52,10 +57,15 @@ def generate_image_from_ipl(ipl_code, image_path):
 
         if command_type.startswith("<STX>H"):
             # Handle text commands
-            x = int(parameters[1].split(",")[1][1:])
-            y = int(parameters[2].split(",")[0][1:])
-            text = parameters[-1].split(",")[1][1:-1]  # Extract text value within { }
-            draw.text((x, y), text, fill="black", font=font)
+            x = int(parameters[1].split(",")[0][1:])
+            y = int(parameters[1].split(",")[1])
+            text = parameters[-2].split(",")[1]
+            if parameters[-2] == 'h01':
+                direction = 'ttb'
+            else:
+                direction = 'ttb'
+                print(direction)
+            draw.text((x, y), text, fill="black", font=font, directino = direction)
 
         elif command_type.startswith("<STX>L"):
             # Handle line commands
@@ -72,3 +82,5 @@ ipl_code = ipl_format_for_Epp_1_5_ml
 
 image_path = "generated_label.png"
 generate_image_from_ipl(ipl_code, image_path)
+
+
